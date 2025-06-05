ca6c26bcc5bda4daf6f7e70be088529a491c713b
riya chavan <riyabchavan.409@email.com>
2025-06-05 11:15:23 +0530

    impl callback

--- world.js
+++ world.js
@@ -6,9 +6,18 @@
 
 
 
-const sq = function(x=5){
-    let ans = 0;
-    ans = x*x;
-    return ans;
+// const sq = function(x=5){
+    // let ans = 0;
+    // ans = x*x;
+    // return ans;
+// }
+// console.log("the ans is:" + sq(9));
+
+function great(name, cb){
+    console.log("heyy"+ name + "!");
+    cb();
 }
-console.log("the ans is:" + sq(9));
+function gdb(){
+    console.log("morninn!!");
+}
+great("riya", gdb);
